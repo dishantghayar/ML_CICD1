2dfd5c054890f620285c33c6e3c807645f71a20a
Dishant Ghayar <155740340+dishantghayar@users.noreply.github.com>
2026-01-06 10:51:16 +0530

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,12 +1,17 @@
 import os
+import sys
 import joblib
+
+# Add project root to Python path
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, ROOT_DIR)
+
 from src.train import train_model
 
 
 def test_model_training():
     train_model()
     assert os.path.exists("models/model.pkl")
-    
-    
+
     model = joblib.load("models/model.pkl")
     assert hasattr(model, "predict")
